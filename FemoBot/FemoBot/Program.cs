336928b8cb8c9ab50e8b16b567428c21d8844c23
Ryan <faraeinkarimi@gmail.com>
2019-05-31 10:59:26 +0200

    a ping function and normal token login using environment

---
+++ FemoBot/FemoBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace FemoBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceivedAsync;
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                Console.WriteLine("User sent a message");
+                await message.Channel.SendMessageAsync("pong!");
+            }
+        }
+        
+
+
+    }
+}
